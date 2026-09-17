b63f1ad6de12c9555a8ef4828f7842de02d1315b
kenjiwho <kenji.opdam@gmail.com>
2026-09-17 00:16:09 +0000

    temp save

--- our_runs/task2.sh
+++ our_runs/task2.sh
@@ -3,7 +3,7 @@ export OMP_NUM_THREADS=1
 # setup environment
 command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 [ -d ".venv" ] || uv venv
-uv sync --extra gpu --group dev
+uv sync --extra gpu
 source .venv/bin/activate
 
 # -----------------------------------------------------------------------------
@@ -13,7 +13,7 @@ source .venv/bin/activate
 # #    `wandb login`
 # # 2) Set the WANDB_RUN environment variable when running this script, e.g.:
 # #    `WANDB_RUN=d26 bash speedrun.sh`
-WANDB_RUN=cpu_run1
+WANDB_RUN=cpu_run_lightning2
 if [ -z "$WANDB_RUN" ]; then
     # by default use "dummy" : it's handled as a special case, skips logging to wandb
     WANDB_RUN=dummy
@@ -27,7 +27,7 @@ python -m nanochat.dataset -n 5
 
 # pre-train transformer
 # torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=2 --save-every=100 --eval-every=100 --target-param-data-ratio=8 --device-batch-size=16 --run=$WANDB_RUN
-torchrun --standalone --nproc_per_node=2 -m scripts.base_train -- --depth=2 --save-every=100 --eval-every=1 --target-param-data-ratio=8 --device-batch-size=2 --run=$WANDB_RUN
+torchrun --standalone --nproc_per_node=1 -m scripts.base_train -- --depth=2 --save-every=10 --eval-every=10 --target-param-data-ratio=8 --device-batch-size=2 --run=$WANDB_RUN --window-pattern=L
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
-torchrun --standalone --nproc_per_node=2 -m scripts.base_eval -- --device-batch-size=2
+torchrun --standalone --nproc_per_node=1 -m scripts.base_eval -- --device-batch-size=2
 
