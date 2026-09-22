92fefc07571f80f5c44ec61a0d4a4a1c8026aa7d
Alex3124536745869708 <mrkorting@gmail.com>
2026-09-22 17:58:52 +0200

    task3 done

--- our_runs/task3.sh
+++ our_runs/task3.sh
@@ -1,21 +1,21 @@
-# # when using gpu ==================
-# export OMP_NUM_THREADS=1
-
-# # setup environment
-# command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
-# [ -d ".venv" ] || uv venv
-# uv sync --extra gpu --group dev
-# source .venv/bin/activate
-
-# when using cpu ==================
-export OMP_NUM_THREADS=8
+# when using gpu ==================
+export OMP_NUM_THREADS=1
 
 # setup environment
 command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 [ -d ".venv" ] || uv venv
-uv sync
+uv sync --extra gpu --group dev
 source .venv/bin/activate
-uv pip install setuptools
+
+# # when using cpu ==================
+# export OMP_NUM_THREADS=8
+
+# # setup environment
+# command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
+# [ -d ".venv" ] || uv venv
+# uv sync
+# source .venv/bin/activate
+# uv pip install setuptools
 
 # -----------------------------------------------------------------------------
 # # wandb setup
@@ -37,10 +37,10 @@ export PYTORCH_ALLOC_CONF=expandable_segments:True
 # torchrun --standalone --nproc_per_node=2 -m scripts.chat_eval -- -i stage1 -a "ARC-Easy|ARC-Challenge|GSM8K"
 
 # # sft
-# # when using gpu
-# torchrun --standalone --nproc_per_node=2 -m scripts.chat_sft -- --run=$WANDB_RUN
-# # torchrun --standalone --nproc_per_node=2 -m scripts.chat_eval -- -i sft -a "ARC-Easy|ARC-Challenge|GSM8K"
+# when using gpu
+# torchrun --standalone --nproc_per_node=1 -m scripts.chat_sft -- --run=$WANDB_RUN --load-optimizer=0 --device-batch-size=16 #device batch size was 32
+torchrun --standalone --nproc_per_node=1 -m scripts.chat_eval -- -i sft -a "ARC-Easy|ARC-Challenge|GSM8K"
 
 # when using cpu
-python3 -m scripts.chat_sft --run=$WANDB_RUN
+# python3 -m scripts.chat_sft --run=$WANDB_RUN
 # torchrun --standalone --nproc_per_node=1 -m scripts.chat_eval -- -i sft -a "ARC-Easy|ARC-C
