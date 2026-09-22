209ae573ea4008bfed642b3c7080dda61d6015e6
Alex3124536745869708 <mrkorting@gmail.com>
2026-09-22 17:59:25 +0200

    done

--- our_runs/task3.sh
+++ our_runs/task3.sh
@@ -1,4 +1,3 @@
-# when using gpu ==================
 export OMP_NUM_THREADS=1
 
 # setup environment
@@ -6,16 +5,6 @@ command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 [ -d ".venv" ] || uv venv
 uv sync --extra gpu --group dev
 source .venv/bin/activate
-
-# # when using cpu ==================
-# export OMP_NUM_THREADS=8
-
-# # setup environment
-# command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
-# [ -d ".venv" ] || uv venv
-# uv sync
-# source .venv/bin/activate
-# uv pip install setuptools
 
 # -----------------------------------------------------------------------------
 # # wandb setup
@@ -32,15 +21,11 @@ fi
 
 export PYTORCH_ALLOC_CONF=expandable_segments:True
 
-# mid-learning
-# torchrun --standalone --nproc_per_node=2 -m scripts.chat_sft_mid -- --run=$WANDB_RUN
-# torchrun --standalone --nproc_per_node=2 -m scripts.chat_eval -- -i stage1 -a "ARC-Easy|ARC-Challenge|GSM8K"
+# mid-learning Using 2 gpus
+torchrun --standalone --nproc_per_node=2 -m scripts.chat_sft_mid -- --run=$WANDB_RUN
+torchrun --standalone --nproc_per_node=2 -m scripts.chat_eval -- -i stage1 -a "ARC-Easy|ARC-Challenge|GSM8K"
 
 # # sft
-# when using gpu
-# torchrun --standalone --nproc_per_node=1 -m scripts.chat_sft -- --run=$WANDB_RUN --load-optimizer=0 --device-batch-size=16 #device batch size was 32
+# when using gpu Using 1 gpu
+torchrun --standalone --nproc_per_node=1 -m scripts.chat_sft -- --run=$WANDB_RUN --load-optimizer=0 --device-batch-size=16 #device batch size was 32
 torchrun --standalone --nproc_per_node=1 -m scripts.chat_eval -- -i sft -a "ARC-Easy|ARC-Challenge|GSM8K"
-
-# when using cpu
-# python3 -m scripts.chat_sft --run=$WANDB_RUN
-# torchrun --standalone --nproc_per_node=1 -m scripts.chat_eval -- -i sft -a "ARC-Easy|ARC-C
